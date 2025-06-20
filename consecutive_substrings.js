980ba83045e05aa76389265265292baec6359bc3
Damian Metheny <metheny555@outlook.com>
2025-06-20 17:42:44 -0400

    lab

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,5 +1,13 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  if (!string) return [];
+
+  const results = [];
+  for (let start = 0; start < string.length; start++) {
+    for (let end = start + 1; end <= string.length; end++) {
+      results.push(string.slice(start, end));
+    }
+  }
+  return results;
 }
 
 if (require.main === module) {
